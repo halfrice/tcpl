e52d2a490ce0841d9db6d8e2358c4e58156707e6
halfrice <halfrice@pm.me>
2019-05-22 12:38:22 -0700

    1-2-escape-sequence

---
+++ c1/1-1-hello-world.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+// Exercise 1-1. Run the "hello, world" program on your system. Experiement with leaving out parts of the program, to see what error messages you get.
+
+int main()
+{
+  printf("Konichiwa Sekai\n");
+}
---
+++ c1/1-2-escape-sequence.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Exercise 1-2. Experiement to find out what happens when printf's argument string contains \c, where c is some character not listed:
+// \t = tab
+// \b = backspace
+// \" = double quote
+// \\ = backslash
+
+int main()
+{
+  printf("Konichiwa \vSekai\n");
+}
