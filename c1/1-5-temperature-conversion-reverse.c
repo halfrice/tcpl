6bf16686bbb15c77759ad183951c78727a22b5da
halfrice <halfrice@pm.me>
2019-05-22 23:54:52 -0700

    Symbolic constants

--- c1/1-5-temperature-conversion-reverse.c
+++ c1/1-5-temperature-conversion-reverse.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 // Exercise 1-5. Modify the temperature conversion program to print the table in reverse order, that is, from 300 degrees to 0.
 
 int main()
 {
   float fahr, celsius;
-  int lower, upper, step;
 
-  lower = 0;
-  upper = 300;
-  step = 20;
-
-  printf("---------------------------\n");
-  printf("|    Celsius | Fahrenheit |\n");
-  printf("|------------+------------|\n");
-  for (celsius = upper; celsius >= lower; celsius = celsius - step)
+  printf("------------------------\n");
+  printf("| Fahrenheit | Celsius |\n");
+  printf("|------------+---------|\n");
+  for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
   {
-    fahr = ((9.0 / 5.0) * celsius) + 32.0;
-    printf("| %10.1f | %10.1f |\n", celsius, fahr);
+    celsius = (5.0 / 9.0) * (fahr - 32.0);
+    printf("| %10.1f | %7.1f |\n", fahr, celsius);
   }
-  printf("---------------------------\n");
+  printf("------------------------\n");
 }
